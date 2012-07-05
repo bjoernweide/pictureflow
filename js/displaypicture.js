763c66859914ddf10c3bfc0f030d1605e15224c3
Bjoern Weide <mail@bjoernweide.com>
2012-07-05 19:32:04 +0200

    Added lightbox closebutton.

--- js/displaypicture.js
+++ js/displaypicture.js
@@ -6,19 +6,24 @@
 	$.fn.showPicture = function() {
 		
 		var target = this;
+		
+		//set photo id in global var
+		var id = target.parents("li").attr("id");
+		selectedPhoto = id;		
+		
 		var url = target.css("background-image");
 		url = url.replace("preview", "full");
 		var realWidth = target.data("width");
 		var realHeight = target.data("height");
 		var isPhotoLandscape = (realWidth > realHeight);
 		var ratio = realHeight / realWidth;
+		
 		var canvas = $("#canvas");
-		
 		$(".photo").addClass("blur");
-		
 		canvas.css("height", $(document).height());
 		canvas.css("width", $(document).width());
 		
+		var lightboxNavigation = $("#lightbox-navigation");
 		var lightboxContainer = $("#lightbox-container");
 		lightboxContainer.css("height", $(document).height());
 		lightboxContainer.css("width", $(document).width());
@@ -37,19 +42,22 @@
 		lightbox.css("width", width);
 		var offset = $(window).scrollTop();
 		var lightBoxTop = (offset + ($(window).height() - height - 30) / 2);
+		var lightBoxLeft = ($(window).width() - width) / 2;
 		lightbox.css("top", lightBoxTop);
-		lightbox.css("left", ($(window).width() - width) / 2);
+		lightbox.css("left", lightBoxLeft);
 		lightbox.css("background-image", url);
 		
-		var lightboxNavigation = $("#lightbox-navigation");
-		lightboxNavigation.css("top", lightBoxTop + height + 5);
+		var closeLink = $("#closeLink");
+		lightboxNavigation.css("top", lightBoxTop + height + 10);
+		closeLink.css("top", lightBoxTop);
+		closeLink.css("left", lightBoxLeft + width + 10);
+		
 		lightbox.click(function() {
-			$(".photo").removeClass("blur");
-			lightbox.fadeOut(200);
-			lightboxNavigation.hide();
-			$("#lightbox-container").fadeOut(200);
+			closeLightBox();
 		});
 		lightbox.show();
-		lightboxNavigation.show();		
+		lightboxNavigation.show();
+		closeLink.show();
 	};
+
 })(jQuery);
